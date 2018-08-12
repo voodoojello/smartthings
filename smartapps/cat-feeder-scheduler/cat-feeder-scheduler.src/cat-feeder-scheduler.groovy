8e784f106f7de3fbc72f82cbd3944407f28731ee
MARK PAGE <m.e.page@gmail.com>
2018-08-12 07:51:50 -0500

    Add third feeding

--- smartapps/cat-feeder-scheduler/cat-feeder-scheduler.src/cat-feeder-scheduler.groovy
+++ smartapps/cat-feeder-scheduler/cat-feeder-scheduler.src/cat-feeder-scheduler.groovy
@@ -4,9 +4,9 @@
  *  Cat Feeder Scheduler
  * ============================================ 
  *
- *  Daily run times and intervals for automatic cat feeder. Allows for two feedings per day
+ *  Daily run times and intervals for automatic cat feeder. Allows for three feedings per day
  *
- *  Copyright (c)2017 Mark Page (mark@very3.net)
+ *  Copyright (c)2018 Mark Page (mark@very3.net)
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
@@ -38,10 +38,13 @@ preferences {
     input "catfeederDevice", "capability.switch", title: "Select device to control cat feeder:", required: true
   }
   section {
-    input "morningFeeding","time",title:"Morning Feeding:", required: true
+    input "firstFeeding","time",title:"First Feeding:", required: true
   }
   section {
-    input "eveningFeeding","time",title:"Evening Feeding:", required: true
+    input "secondFeeding","time",title:"Second Feeding:", required: true
+   }
+  section {
+    input "thirdFeeding","time",title:"Third Feeding:", required: true
    }
   section {
     input "runTime","number",title:"Motor run time in minutes:", required: true
@@ -68,27 +71,38 @@ def updated() {
 def initialize() {
   def tz = location.timeZone
 
-  def morningStartTime  = timeToday(morningFeeding,tz)
-  def morningStartHour  = morningStartTime.format("H",tz)
-  def morningStartMin   = morningStartTime.format("m",tz).toInteger() + minOffset.toInteger()
-  def morningStopMin    = morningStartMin.toInteger() + runTime.toInteger() + minOffset.toInteger()
-  def morningStartCron  = "${secOffset} ${morningStartMin} ${morningStartHour} ? * *"
-  def morningStopCron   = "${secOffset} ${morningStopMin} ${morningStartHour} ? * *"
+  def firstStartTime  = timeToday(firstFeeding,tz)
+  def firstStartHour  = firstStartTime.format("H",tz)
+  def firstStartMin   = firstStartTime.format("m",tz).toInteger() + minOffset.toInteger()
+  def firstStopMin    = firstStartMin.toInteger() + runTime.toInteger() + minOffset.toInteger()
+  def firstStartCron  = "${secOffset} ${firstStartMin} ${firstStartHour} ? * *"
+  def firstStopCron   = "${secOffset} ${firstStopMin} ${firstStartHour} ? * *"
 
-  schedule("${morningStartCron}", firstFeedingOn)
-  schedule("${morningStopCron}", firstFeedingOff)
-  log.debug "CRON INIT (First Feeding): [${morningStartCron}] to [${morningStopCron}]"
+  schedule("${firstStartCron}", firstFeedingOn)
+  schedule("${firstStopCron}", firstFeedingOff)
+  log.debug "CRON INIT (First Feeding): [${firstStartCron}] to [${firstStopCron}]"
 
-  def eveningStartTime  = timeToday(eveningFeeding,tz)
-  def eveningStartHour  = eveningStartTime.format("H",tz)
-  def eveningStartMin   = eveningStartTime.format("m",tz).toInteger() + minOffset.toInteger()
-  def eveningStopMin    = eveningStartMin.toInteger() + runTime.toInteger() + minOffset.toInteger()
-  def eveningStartCron  = "${secOffset} ${eveningStartMin} ${eveningStartHour} ? * *"
-  def eveningStopCron   = "${secOffset} ${eveningStopMin} ${eveningStartHour} ? * *"
+  def secondStartTime = timeToday(secondFeeding,tz)
+  def secondStartHour = secondStartTime.format("H",tz)
+  def secondStartMin  = secondStartTime.format("m",tz).toInteger() + minOffset.toInteger()
+  def secondStopMin   = secondStartMin.toInteger() + runTime.toInteger() + minOffset.toInteger()
+  def secondStartCron = "${secOffset} ${secondStartMin} ${secondStartHour} ? * *"
+  def secondStopCron  = "${secOffset} ${secondStopMin} ${secondStartHour} ? * *"
 
-  schedule("${eveningStartCron}", secondFeedingOn)
-  schedule("${eveningStopCron}", secondFeedingOff)
-  log.debug "CRON INIT (Second Feeding): [${eveningStartCron}] to [${eveningStopCron}]"
+  schedule("${secondStartCron}", secondFeedingOn)
+  schedule("${secondStopCron}", secondFeedingOff)
+  log.debug "CRON INIT (Second Feeding): [${secondStartCron}] to [${secondStopCron}]"
+
+  def thirdStartTime  = timeToday(thirdFeeding,tz)
+  def thirdStartHour  = thirdStartTime.format("H",tz)
+  def thirdStartMin   = thirdStartTime.format("m",tz).toInteger() + minOffset.toInteger()
+  def thirdStopMin    = thirdStartMin.toInteger() + runTime.toInteger() + minOffset.toInteger()
+  def thirdStartCron  = "${secOffset} ${thirdStartMin} ${thirdStartHour} ? * *"
+  def thirdStopCron   = "${secOffset} ${thirdStopMin} ${thirdStartHour} ? * *"
+
+  schedule("${thirdStartCron}", thirdFeedingOn)
+  schedule("${thirdStopCron}", thirdFeedingOff)
+  log.debug "CRON INIT (third Feeding): [${thirdStartCron}] to [${thirdStopCron}]"
 }
 
 def firstFeedingOn() {
@@ -104,5 +118,13 @@ def secondFeedingOn() {
   sendPush("${msgText}")
 }
 def secondFeedingOff() {
+  catfeederDevice.off()
+}
+
+def thirdFeedingOn() {
+  catfeederDevice.on()
+  sendPush("${msgText}")
+}
+def thirdFeedingOff() {
   catfeederDevice.off()
 }
