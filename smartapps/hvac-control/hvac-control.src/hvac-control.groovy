32218782702ebfe5c83e6a46b759545171d6fd90
Mark Page <m.e.page@gmail.com>
2019-11-25 09:33:14 -0600

    Added setmode and setpoints from local source

--- smartapps/hvac-control/hvac-control.src/hvac-control.groovy
+++ smartapps/hvac-control/hvac-control.src/hvac-control.groovy
@@ -1,30 +1,30 @@
-/**
- *
- * ============================================
- *  PWS JSON Proxy - HVAC Control
- * ============================================
- *
- *  Control HVAC based on presence and various climate levels from the very3 Ambient PWS JSON proxy
- *
- *  Copyright (c)2017 Mark Page (mark@very3.net)
- *
- *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- *  in compliance with the License. You may obtain a copy of the License at:
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
- *  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License
- *  for the specific language governing permissions and limitations under the License.
- *
- */
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Ambient HVAC Control for SmartThings
+//  Copyright (c)2019-2020 Mark Page (mark@very3.net)
+//  Modified: Sun Nov 24 08:28:56 CST 2019
+//
+//  Control HVAC based via published capabilities of the very3 Ambient PWS Device Handler.
+//
+//      https://github.com/voodoojello/smartthings/tree/master/devicetypes/apws-device-handler
+//
+//  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+//  in compliance with the License. You may obtain a copy of the License at:
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is
+//  distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and limitations under the License.
+//
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 definition (
   name: "HVAC Control",
-  version: "17.11.05.02",
+  version: "19.11.24.6",
   namespace: "hvac-control",
   author: "Mark Page",
-  description: "Control HVAC based on presence and various climate levels from the very3 Ambient PWS JSON. (v19.11.24.6)",
+  description: "Control HVAC based via published capabilities of the very3 Ambient PWS Device Handler",
   singleInstance: true,
   category: "SmartThings Internal",
   iconUrl: "https://raw.githubusercontent.com/voodoojello/smartthings/master/very3-256px.png",
@@ -45,6 +45,12 @@ def mainPage() {
 
     section ("Select Thermostats") {
       input "thermostats", "capability.thermostat", title: "Select thermostats:", multiple: true, required: true
+    }
+    
+    section ("Select Thermostat Setpoint Source") {
+      input "thermostatModeValue", "capability.thermostatMode", title: "Mode Source:", required: true
+      input "thermostatCoolingSetpointValue", "capability.thermostatCoolingSetpoint", title: "Cooling Setpoint Source:", required: true
+      input "thermostatHeatingSetpointValue", "capability.thermostatHeatingSetpoint", title: "Heating Setpoint Source:", required: true
     }
 
     section ("Day/Night Temperature Presets") {
@@ -86,95 +92,126 @@ def mainPage() {
   }
 }
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 def installed() {
-  log.debug "HVACC: Installed with settings: ${settings}"
+  logger('info','installed',"Installed with settings: ${settings}")
   initialize()
 }
 
 def updated() {
-  log.debug "HVACC: Updated with settings: ${settings}"
+  logger('info','updated',"Updated with settings: ${settings}")
+  
+  def getURL  = "https://pws.very3.net/?_k="+appKey+"&_dc="+dayCool+"&_nc="+nightCool+"&_dh="+dayHeat+"&_nh="+nightHeat+"&_ct="+modeThresCool+"&_ht="+modeThresHeat+"&_nb="+nightStart+"+&_ne="+nightStop+"&_tt="+tweakTemp+"&_th="+tweakHumi
+  logger('debug','updated',"[getURL] ${getURL}")
+  def pwsData = updateServer(getURL)
+  
+  logger('trace','updated',"PWS night_mode: ${pwsData.hvac.nightmode}")
+  logger('trace','updated',"PWS set_temp: ${pwsData.hvac.set_temp}")
+  logger('trace','updated',"PWS adj_temp: ${pwsData.hvac.adj_temp}")
+  logger('trace','updated',"PWS diff_temp: ${pwsData.hvac.diff_temp}")
+  logger('trace','updated',"PWS apdiff_temp: ${pwsData.hvac.apdiff_temp}")
+  logger('trace','updated',"PWS hvac_mode: ${pwsData.hvac.hvac_mode}")
+ 
   unsubscribe()
   initialize()
 }
 
 def initialize() {
-  mainRouter()
-  runEvery30Minutes(mainRouter)
+  state.logMode   = 0
+  state.logHandle = 'hvacc-sa'
+  
+  poll()
+  runEvery5Minutes(poll)
 }
 
-def mainRouter() {
-  log.info "HVACC: Starting..."
-  def getUrl  = "https://pws.very3.net/?_k="+appKey+"&_dc="+dayCool+"&_nc="+nightCool+"&_dh="+dayHeat+"&_nh="+nightHeat+"&_ct="+modeThresCool+"&_ht="+modeThresHeat+"&_nb="+nightStart+"+&_ne="+nightStop+"&_tt="+tweakTemp+"&_th="+tweakHumi
-  def pwsData = fetchJSON(getUrl)
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+def poll() {
+  logger('info','poll',"Starting...")
   
-  def set_temp  = pwsData.hvac.set_temp
-  def adj_temp  = pwsData.hvac.adj_temp
-  def hvac_mode = pwsData.hvac.hvac_mode
+  def hvac_mode = thermostatModeValue.latestValue("thermostatMode")
   def currMode  = location.mode
+  def adj_temp  = ''
   
-  if (currMode.toLowerCase() == "away" && hvac_mode == "heat") {
-  	adj_temp = awayHeatTemp
-  }
-
-  if (currMode.toLowerCase() == "away" && hvac_mode == "cool") {
-  	adj_temp = awayCoolTemp
-  }
-
-  if (hvac_mode == "cool" && thermHoldSwitch.currentSwitch == 'off') {
-    thermostats.cool()
-    thermostats.fanAuto()
-    thermostats.setCoolingSetpoint(adj_temp)
-  }
-
   if (hvac_mode == "heat" && thermHoldSwitch.currentSwitch == 'off') {
+  	adj_temp = thermostatHeatingSetpointValue.latestValue("thermostatHeatingSetpoint")
+    
+    if (currMode.toLowerCase() == "away") {
+      adj_temp = awayHeatTemp
+    }
+    
     thermostats.heat()
     thermostats.fanAuto()
     thermostats.setHeatingSetpoint(adj_temp)
   }
+  
+  if (hvac_mode == "cool" && thermHoldSwitch.currentSwitch == 'off') {
+  	adj_temp = thermostatHeatingSetpointValue.latestValue("thermostatCoolingSetpoint")
+    
+    if (currMode.toLowerCase() == "away") {
+      adj_temp = awayCoolTemp
+    }
+    
+    thermostats.cool()
+    thermostats.fanAuto()
+    thermostats.setCoolingSetpoint(adj_temp)
+  }
+  
+  logger('debug','poll',"hvac_mode: ${hvac_mode}, adj_temp: ${adj_temp}, currMode: ${currMode}, thermHoldSwitch: ${thermHoldSwitch.currentSwitch}")
 
   if (thermHoldSwitch.currentSwitch == 'on') {
-    sendNotificationEvent("HVACC: thermHoldSwitch is ON, no action taken.")
+    sendNotificationEvent("${state.logHandle}: thermHoldSwitch is ON, no action taken.")
+    logger('info','poll',"thermHoldSwitch is ON, no action taken.")
   }
   else {
-    sendNotificationEvent("HVACC: Set HVAC mode to ${hvac_mode}, set temperature to ${pwsData.hvac.set_temp} (${adj_temp} adjusted, ${currMode} mode, Night ${pwsData.hvac.nightmode}). OS Temperature: ${pwsData.pws.tempf}°. OS Humidity: ${pwsData.pws.humidity}%.")
+    sendNotificationEvent("${state.logHandle}: Set HVAC mode to ${hvac_mode}, set temperature to ${adj_temp}")
+    logger('info','poll',"Set HVAC mode to ${hvac_mode}, set temperature to ${adj_temp}")
   }
 
-  log.trace "HVACC [${thermostats[0].label}] latestTempValue: ${thermostats[0].latestValue("temperature")}"
-  log.trace "HVACC [${thermostats[0].label}] latestModeValue: ${thermostats[0].latestValue("thermostatMode")}"
-  log.trace "HVACC [${thermostats[0].label}] latestCoolingSetPoint: ${thermostats[0].latestValue("coolingSetpoint")}"
+  logger('debug','poll',"[Device Handler] thermostatModeValue: ${thermostatModeValue.latestValue("thermostatMode")}")
+  logger('debug','poll',"[Device Handler] thermostatCoolingSetpointValue: ${thermostatCoolingSetpointValue.latestValue("thermostatCoolingSetpoint")}")
+  logger('debug','poll',"[Device Handler] thermostatHeatingSetpointValue: ${thermostatHeatingSetpointValue.latestValue("thermostatHeatingSetpoint")}")
 
-  log.trace "HVACC [${thermostats[1].label}] latestTempValue: ${thermostats[1].latestValue("temperature")}"
-  log.trace "HVACC [${thermostats[1].label}] latestModeValue: ${thermostats[1].latestValue("thermostatMode")}"
-  log.trace "HVACC [${thermostats[1].label}] latestHeatingSetPoint: ${thermostats[1].latestValue("heatingSetpoint")}"
+  logger('debug','poll',"[${thermostats[0].label}] latestTempValue: ${thermostats[0].latestValue("temperature")}")
+  logger('debug','poll',"[${thermostats[0].label}] latestModeValue: ${thermostats[0].latestValue("thermostatMode")}")
+  logger('debug','poll',"[${thermostats[0].label}] latestCoolingSetPoint: ${thermostats[0].latestValue("coolingSetpoint")}")
 
-  log.info "HVACC thermHoldSwitchState: ${thermHoldSwitch.currentSwitch}"
-  log.info "HVACC set_temp: ${set_temp}"
-  log.info "HVACC night_mode: ${pwsData.hvac.nightmode}"
-  log.info "HVACC PWS outtemp: ${pwsData.pws.tempf}"
-  log.info "HVACC PWS outhumi: ${pwsData.pws.humidity}"
-  log.info "HVACC PWS apptemp: ${pwsData.pws.feelsLike}"
-  log.info "HVACC PWS set_temp: ${pwsData.hvac.set_temp}"
-  log.info "HVACC PWS adj_temp: ${pwsData.hvac.adj_temp}"
-  log.info "HVACC PWS diff_temp: ${pwsData.hvac.diff_temp}"
-  log.info "HVACC PWS apdiff_temp: ${pwsData.hvac.apdiff_temp}"
-  log.info "HVACC PWS hvac_mode: ${pwsData.hvac.hvac_mode}"
+  logger('debug','poll',"[${thermostats[1].label}] latestTempValue: ${thermostats[1].latestValue("temperature")}")
+  logger('debug','poll',"[${thermostats[1].label}] latestModeValue: ${thermostats[1].latestValue("thermostatMode")}")
+  logger('debug','poll',"[${thermostats[1].label}] latestHeatingSetPoint: ${thermostats[1].latestValue("heatingSetpoint")}")
 }
 
-def fetchJSON(pwsURI) {
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+def updateServer(pushURI) {
   def params = [
-    uri: pwsURI,
+    uri: pushURI,
     format: 'json',
     contentType: 'application/json'
   ]
 
   try {
     httpGet(params) { response ->
-      log.info "HVACC: httpGet (OK)"
+      logger('info','updateServer',"httpGet (OK)")
+      logger('debug','updateServer',"${response.data}")
       return response.data
     }
   }
   catch (e) {
-    log.error "HVACC: httpGet (ERROR: $e)"
+    logger('error','updateServer',"httpGet (ERROR: ${e})")
     return "ERROR: $e"
+  }
+}
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+private logger(type,loc,msg) {
+  // type: error, warn, info, debug, trace
+  if ("${type}" == 'info') {
+    log."${type}" "${state.logHandle} [${loc}]: ${msg}"
+  }
+  else if (state.logMode > 0) {
+    log."${type}" "${state.logHandle} [${loc}]: ${msg}"
   }
 }
